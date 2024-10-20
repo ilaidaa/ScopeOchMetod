7124944d8638d76066d911f350dddcfd7c1f28f0
ilaidaa <ilaida.e@hotmail.com>
2024-10-20 16:48:44 +0200

    Add project files.

---
+++ ScopeOchMetod/Bilfabriken.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeOchMetod
+{
+    internal class Bilfabriken
+    {
+        //Skapa en global variabel i klassen Bilfabrik så att alla andra metoder kan nå variabeln. 
+        //Den har i uppgift att räkna ut hur mycket tid varje slumpmässiga process tar
+        //Varför static? : när vi kör OOP finns inte static men eftersom alla metoder som skrivs nedan kommer vara static måste variabeln också vara det så att metoderna når den.
+        public static int time = 0;
+
+
+
+
+
+
+
+
+
+
+        //Metod 1 - Basmetoden
+        //Metoden som bygger bilarna genom att använda alla andra submetoder
+        public static void BuildingCars()
+        {
+            //Fråga användaren hur många bilar hen vill skapa
+            Console.Write("Skriv in antal bilar du vil skapa: ");
+            //Spara användarens inmatning
+            int amount = int.Parse(Console.ReadLine());
+            //Design för att skapa mellanrum mellan frågan och bilarna som ska skrivas ut
+            Console.WriteLine();
+
+
+
+            //Skapa en for loop som loopar igenom antalet bilar som användaren skrivit in
+            for (int i = 1; i < amount + 1; i++) //Egentligen ska det stå int i = 0; i < amount; i++
+                                                 //Anledningen till att det inte gör det är att vi vill att det på rad 41 inte ska stå bil nr 0: första gången loopen körs. 
+                                                 //Maskinen startar från 0 och amount får + 1 för att datorprogram´alltid börjar från index 0.
+            {
+                Console.WriteLine("Bil nr " + i + ": ");
+                BuildUnderCar();
+                BuildWheels();
+                BuildEngine();
+                BuildColour();
+                Console.WriteLine("Tid: antal minuter för att skapa just denna bil är " + time);
+
+                Console.WriteLine(); //Mellanrum för design
+
+            }
+
+            Console.ReadKey(); //För att programmet inte ska avslutas direkt
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //Metod 2 - Submetod
+        //Bygger slumpmässig underrede
+        public static void BuildUnderCar()
+        {
+            int choice = Random.Shared.Next(0, 2); //Skapa en random variabel som innehåller 2 val, nämligen 0 och 1. det sista talet räknas aldrig så 2 finns ej med
+
+            if (choice == 0) //Om slumptalet är 0 ska följande if sats köras. 
+            {
+                Console.WriteLine("Underrede: rostfritt underede, ");
+                time += 5; //Förkortning på time = time + 5
+                           //Betyder att ett rostfritt underede tar 5 minuter 
+            }
+            else //Om slumptalet är 1 ska följande if sats köras. 
+            {
+                Console.WriteLine("Underrede: underede av stål, ");
+                time += 10;
+            }
+        }
+
+
+
+
+
+
+        //Metod 3 - Submetode
+        //Ger antal hjul slumpmässig 
+        public static void BuildWheels()
+        {
+            int choice = Random.Shared.Next(0, 3); //Slumpmässig variabel som kan ge tre alternativ. Följande: 0, 1, 2 (Räkna aldrig sista talet)
+
+            switch (choice) //När du har fler alternativ än 2 är det bättre att köra med en switch sats ist för if.
+            {
+                case 0: //Om Slumptalet är 0 ska följande gälla:
+                    Console.WriteLine("Hjul: 4 slitna hjul, ");
+                    time += 5;
+                    break;
+                case 1: //Om Slumptalet är 1 ska följande gälla
+                    Console.WriteLine("Hjul:  4 fyrhjulsdrivna däck");
+                    time += 15;
+                    break;
+                case 2: //Om slumptalet är 2 ska följande gälla:
+                    Console.WriteLine("Hjul: 4 vinterdäck.");
+                    time += 10;
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+        //Metod 4 - Subklass
+        //Bygger motor
+        public static void BuildEngine()
+        {
+            int choice = Random.Shared.Next(0, 3); //Slumpmässigt tal som genererar 3 alternativ. Följande: 0, 1, 2 (Räkna aldrig sista talet)
+
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("Motor: 75 hästkrafter + en åsna bakom ratten. ");
+                    time += 5;
+                    break;
+                case 1:
+                    Console.WriteLine("Motor: 170 hästkrafter. ");
+                    time += 10;
+                    break;
+                case 2:
+                    Console.WriteLine("Motor: 550 fkng hästkrafter. ");
+                    time += 15;
+                    break;
+            }
+        }
+
+
+
+
+
+
+
+
+        //Metod 5 - Subklass
+        //Bygger karossen
+        public static void BuildColour()
+        {
+            int choice = Random.Shared.Next(0, 2); //Slumptal som skapar 2 alternativ. Följande: 0, 1
+
+            if (choice == 0) //Eftersom att vi bara har två alternativ att slumpa mellan är en if sats mer passande
+            {
+                Console.WriteLine("Färg: Bilen är svart.");
+                time += 15;
+            }
+            else
+            {
+                Console.WriteLine("Färg: Bilen är vit. ");
+                time += 10;
+            }
+        }
+
+
+    }
+}
---
+++ ScopeOchMetod/DiceGame.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeOchMetod
+{
+    internal class DiceGame
+    {
+        //Global  variabel som ska räkna poäng
+        //Static för att metoderna är static
+
+        static int point = 0;
+
+
+
+
+
+
+
+
+        //Metod 1
+        //Skapar EN tärning 
+        public static int RollDice() //Du måste skriva int ist för void för att du returnerar en int variabel
+        {
+            int choice = Random.Shared.Next(0, 6); //Skapar en slumpvariabel som ska ge 6 olika alternativ: 0, 1, 2, 3, 4, 5 (det sista talet räknas aldrig men det gör inget eftersom maskinen börjar från 0)
+
+            switch (choice) //Skriv in de 6 slumptalen 
+            {
+                case 0:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("| O |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("=====");
+                    break;
+                case 1:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|O  |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|  O|");
+                    Console.WriteLine("=====");
+                    break;
+                case 2:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|O  |");
+                    Console.WriteLine("| O |");
+                    Console.WriteLine("|  O|");
+                    Console.WriteLine("=====");
+                    break;
+                case 3:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("=====");
+                    break;
+                case 4:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("| O |");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("=====");
+                    break;
+                case 5:
+                    Console.WriteLine("=====");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("|O O|");
+                    Console.WriteLine("=====");
+                    break;
+            }
+
+            return choice; //Returnerar tärningsresultatet som ett heltal (0-5)
+                           //Du returnerar alltså vilken summa tärningen som kastatdes är.
+                           //Så om det slumpades fram en 3:a så returnerar du talet 3
+                           //Varför? i metod 2 kan du annars inte spara RollDice som en int variabel (dice1, dice2, dice3)
+                           //Du sparar en tärning (dice1) med den slumptärning som den får från denna metod och som tidigare returnerats som choice
+        }
+
+
+
+        //Metod 2
+        //Anropa den första metoden som ska skriva ut tre tärningar efter varandra
+        //Anropar också den sista metoden för att räkna poäng
+        public static void Draw3Dice()
+        {
+            while (true)
+            {
+                Console.Clear(); //Rensa konsolen varje gång while- loopen ska köras
+
+                //Använd slumptalet som returneras i RollDice metoden och spara den i en variabel som dice1
+                int dice1 = RollDice();
+                int dice2 = RollDice();
+                int dice3 = RollDice();
+
+
+                Console.WriteLine();// Mellanslag mellan tärningarna och poängen
+
+                CalculatePoint(dice1, dice2, dice3);
+
+                Console.ReadKey();
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+        //Metod 3
+        //Används för att räkna ut poäng
+        public static void CalculatePoint(int dice1, int dice2, int dice3)
+        //Du måste också ta in dice1, dice2 och dice3 som parameter för att de deklarerades i metoden innan (Metod 2)
+        {
+            if (dice1 == dice2 && dice2 == dice3) //Om alla tre tärningar visar lika
+            {
+                point += 10; //Lägger till 10 poäng på räknar variabeln.
+                             //Måste vara innan raden efter annars kan inte de 10 poängen som lägg tills räknas med i svaret som ges till användaren
+                Console.WriteLine("Snyggt! Du fick tre lika, vilket blir +10 poäng.");
+                Console.WriteLine("Din nuvarande poäng är: " + point);
+            }
+            else if (dice1 == dice2 || dice1 == dice3 || dice2 == dice3) //Om två tärningar visar lika
+            {
+                point += 3;
+                Console.WriteLine("Snyggt! Du fick två lika, vilket blir +3 poäng.");
+                Console.WriteLine("Din nuvarande poäng är: " + point);
+            }
+            else if (dice1 + dice2 + dice3 < 9) //Om summan av alla tärningar är mindre än 9
+            {
+                point -= 5;
+                Console.WriteLine("Ajdå! Summan av alla tärningar är mindre än 9, vilket blir -5 poäng.");
+                Console.WriteLine("Din nuvarande poäng är: " + point);
+            }
+            else //Om inget av ovanstående händer skrivs bara poängen ut
+            {
+                Console.WriteLine("Din nuvarande poäng är: " + point);
+            }
+
+        }
+
+
+    }
+}
